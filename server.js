6c0811c1293ad6cb0271083eba017777a3ed4e2e
nalini-tls <nalini@teralumensolutions.com>
2026-04-21 17:35:07 +0530

    add login route with bcrypt password verification

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/health', (req,res)=>{
     res.json({status:'ok'})
 })
 
+
 app.post('/register',async (req,res)=>{
     // res.json(req.body.email)
     // res.json(req.body.password)
@@ -18,9 +19,26 @@ app.post('/register',async (req,res)=>{
     const hashedPassword = await bcrypt.hash(password, 10)
     await pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, hashedPassword])
     res.json({email: req.body.email, password: req.body.hashedPassword, message:'User registered successfully'})
-
+    
 })
 
-app.listen(3000, ()=>{
+app.post('/login', async(req,res)=>{
+    const {email, password}=req.body
+    const result=await pool.query('SELECT * FROM users Where email =$1', [email])
+    const user=result.rows[0]
+
+    if(user==null){
+        return res.status(400).json({message:'Invalid credentials'})
+    }else{
+        const isPasswordCorrect=await bcrypt.compare(password, user.password)
+        if(isPasswordCorrect){
+            res.json({message:'Login successful'})
+        }else{
+            res.status(400).json({message:'Invalid credentials'})
+        }
+    }
+})
+
+app.listen (3000, ()=>{
     console.log("server is running on port 3000")
 })
