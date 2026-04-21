c877a635fa184c116ac2ad72410a39588f47954a
nalini-tls <nalini@teralumensolutions.com>
2026-04-21 16:10:31 +0530

    integrated database

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+const pool = require('./db')
+
 const express =require('express')
 const app =express()
 
@@ -7,9 +10,11 @@ app.get('/health', (req,res)=>{
     res.json({status:'ok'})
 })
 
-app.post('/register',(req,res)=>{
+app.post('/register',async (req,res)=>{
     // res.json(req.body.email)
     // res.json(req.body.password)
+    const {email, password} = req.body
+    await pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, password])
     res.json({email: req.body.email, password: req.body.password, message:'User registered successfully'})
 
 })
