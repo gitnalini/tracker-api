ec6df117361587dc099a87359d857b9bbc5f0ee2
nalini-tls <nalini@teralumensolutions.com>
2026-04-22 16:52:39 +0530

    add SSL config for production database

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
-const {Pool }=require ('pg')
-require ('dotenv').config()
+const { Pool } = require('pg')
+require('dotenv').config()
 
-
-
-const pool=new Pool({
-    connectionString: process.env.DATABASE_URL
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
-module.exports=pool
-
-
+module.exports = pool
